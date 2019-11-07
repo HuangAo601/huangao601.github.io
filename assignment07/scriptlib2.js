19815ef981953c5b2e10396f63e5112015fdff64
HuangAo601 <54757256+HuangAo601@users.noreply.github.com>
2019-11-07 13:35:57 +0800

    Update scriptlib2.js

--- assignment07/scriptlib2.js
+++ assignment07/scriptlib2.js
@@ -58,30 +58,45 @@ $(document).ready(function(){
               }); // end .each
               console.log(dataSet);
 
-           $('#table2').DataTable( {
-               data: dataSet,
-               retrieve: true,
-               columns: [
-                   { title: "GL Types",
-                     defaultContent:""},
-                   { title: "Total Amount",
-                       defaultContent:"" },
-               ]
-           } );
-
-           var chart = c3.generate({
-                data: {
-                    columns: dataSet,
-                    type : 'bar'
-                },
-                axis: {
-                  x: {label: 'GL Types'},
-                  y: {label: '# of Items'}
-                },
-                bar: {
-                    title: "# of Items by GL Types:",
-                }
-            });
+              $("button#get_data2").click(function() {
+                var items = [];
+                var i = 0;
+                var airtable_read_endpoint = "https://api.airtable.com/v0/appKIU0zkdHt3AVTL/Roll-up?api_key=keycj6dRwXwYLEjiv";
+                var dataSet = [];
+                $.getJSON(airtable_read_endpoint, function(result) {
+                       $.each(result.records, function(key,value) {
+                           items = [];
+                               items.push(value.fields.Name);
+                               items.push(value.fields.total_items_by_category);
+                               dataSet.push(items);
+                               console.log(items);
+                        }); // end .each
+                        console.log(dataSet);
+          
+                     $('#table2').DataTable( {
+                         data: dataSet,
+                         retrieve: true,
+                         columns: [
+                             { title: "Product",
+                               defaultContent:""},
+                             { title: "Total Amount",
+                                 defaultContent:"" },
+                         ]
+                     } );
+          
+                     var chart = c3.generate({
+                          data: {
+                              columns: dataSet,
+                              type : 'bar'
+                          },
+                          axis: {
+                            x: {label: 'Product'},
+                            y: {label: '# of Items'}
+                          },
+                          bar: {
+                              title: "# of Items by Product Category:",
+                          }
+                      });
          
 
       }); // end .getJSON
