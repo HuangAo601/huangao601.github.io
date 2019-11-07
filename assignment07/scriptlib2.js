a986a4e20d0a4d2a1930ef38e6378f6abe1b4eb5
HuangAo601 <54757256+HuangAo601@users.noreply.github.com>
2019-11-07 12:16:56 +0800

    Update scriptlib2.js

--- assignment07/scriptlib2.js
+++ assignment07/scriptlib2.js
@@ -46,13 +46,13 @@ $(document).ready(function(){
      $("button#get_data2").click(function() {
       var items = [];
       var i = 0;
-      var airtable_read_endpoint = "curl https://api.airtable.com/v0/app8hrXsKLF3PqmzR/Roll_up?api_key=keysBAU1yAr5rXWpV";
+      var airtable_read_endpoint = "curl https://api.airtable.com/v0/app8hrXsKLF3PqmzR/Ingredient?api_key=keysBAU1yAr5rXWpV";
       var dataSet = [];
       $.getJSON(airtable_read_endpoint, function(result) {
              $.each(result.records, function(key,value) {
                  items = [];
-                     items.push(value.fields.Name);
-                     items.push(value.fields.Total);
+                      items.push(value.fields.name);
+                       items.push(value.fields.calories);
                      dataSet.push(items);
                      console.log(items);
               }); // end .each
@@ -62,26 +62,25 @@ $(document).ready(function(){
                data: dataSet,
                retrieve: true,
                columns: [
-                   { title: "Namet",
+                   { title: "Name",
                      defaultContent:""},
                    { title: "Total",
                        defaultContent:"" },
                ]
            } );
-
            var chart = c3.generate({
-                data: {
-                    columns: dataSet,
-                    type : 'bar'
-                },
-                axis: {
-                  x: {label: 'GL_Type'},
-                  y: {label: '# of items'}
-                },
-                bar: {
-                    title: "# of Items by GL Types:",
-                }
-            });
+            data: {
+                columns: dataSet,
+                type : 'bar'
+            },
+            axis: {
+              x: {label: 'Product'},
+              y: {label: '# of Items'}
+            },
+            bar: {
+                title: "# of Items by Product Category:",
+            }
+        });
 
       }); // end .getJSON
 
